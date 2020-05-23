b13638d3eec61317313f622987839365b474a12b
Sinésio Neto <sn.logbackup@gmail.com>
2020-05-23 19:40:02 -0300

    feat(pages/home): request data from Github API
    After request data from API, the module will print
    the data on console.log. If the get a bad request will
    throw a error massage to client.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+	const [ user, setUser ] = useState('');
+	const [ error, setError ] = useState(false);
+
+	const handleRepos = async () => {
+		const response = axios.get(`https:/api.github.com/users/${user}/repos`)
+		response
+			.then(result => {
+				setError(false);
+				console.log(result.data);
+			})
+			.catch(err => setError(true));
+	};
+
 	return (
-		<h1>Home</h1>
+		<>
+			<div>
+				<input
+					type='text'
+					placeholder='Usuário'
+					value={user}
+					onChange={e => setUser(e.target.value)}
+				/>
+				<button
+					type='button'
+					onClick={handleRepos}
+				>
+				Pesquisar
+				</button>
+			</div>
+			{ error ? <span>Ocorreu um erro. Tente Novamente.</span> : '' }
+		</>
 	);
 };
 
