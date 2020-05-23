8a443bac353b6b1953e625901f831da1dd9f5971
Sinésio Neto <sn.logbackup@gmail.com>
2020-05-23 20:07:20 -0300

    feat(routes): define Repositories page routes

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Repositories from './pages/Repositories';
 
 const Routes = () => {
 	return (
 		<BrowserRouter>
 			<Switch>
-				<Route path='/' component={Home} />
+				<Route path='/' exact component={Home} />
+				<Route path='/repositories' component={Repositories} />
 			</Switch>
 		</BrowserRouter>
 	);
