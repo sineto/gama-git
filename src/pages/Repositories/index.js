3a162a479d7a5239f2651feb18914663c27f3165
Sinésio Neto <sn.logbackup@gmail.com>
2020-05-23 21:26:34 -0300

    feat(pages/repositories): implemente repositories page
    List all repositories fetched from API and hosted on
    local storage. Allow to see on Repositories page.

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Repositories = () => {
+	const [ repositories, setRepositories ] = useState([]);
+	const history = useHistory();
+
+	useEffect(() => {
+		let data = localStorage.getItem('repositories');
+
+		if (data === null) {
+			history.push('/');
+		}
+
+		data = JSON.parse(data);
+		setRepositories(data);
+		localStorage.clear();
+	}, [history]);
+
 	return (
-		<h1>Repositories</h1>
+		<>
+			<h1>Repositories</h1>
+			<ul>
+				{
+					repositories.map(repository => {
+						return <li>{ repository }</li>
+					})
+				}
+			</ul>
+		</>
 	);
 };
 
